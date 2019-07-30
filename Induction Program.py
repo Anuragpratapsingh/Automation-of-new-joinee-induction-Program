4b017a00a94ca38002413d39e28ee6da8e489849
Anuragpratapsingh <53467402+Anuragpratapsingh@users.noreply.github.com>
2019-07-30 14:19:48 +0530

    Create Induction Program.py

---
+++ Induction Program.py
@@ -0,0 +1,527 @@
+
+
+16 of about 57
+(no subject)
+
+ANURAG PRATAP SINGH <f20160790@goa.bits-pilani.ac.in>
+Attachments
+Wed, Jul 11, 2018, 11:43 AM
+to DEVANSH
+
+
+Attachments area
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+print('\n________Welcome to Birla cellulosic New joinee induction program________\n')
+name=input("Enter the new candidate's name\n")
+other=input("Enter some other information about the candidate\nPlease enter it in closed brackets\n")
+detail=name+other
+day=int(input("Enter the day of starting the induction program\neg.For Monday enter 1\nFor Tuesday enter 2\nFor wednesday enter 3 and so on\n"))
+if day==1:
+    realday="Monday"              
+elif day==2:
+    realday="Tuesday"
+elif day==3:
+    realday="Wednesday"
+elif day==4:
+    realday="Thursday"
+elif day==5:
+    realday="Friday"
+elif day==6:
+    realday="Saturday"
+elif day==8:
+    realday="Monday"
+elif day==9:
+    realday="Tuesday"
+elif day==10:
+    realday="Wednesday"
+elif day==11:
+    realday="Thursday"
+elif day==12:
+    realday="Friday"
+elif day==13:
+    realday="Saturday"
+elif day==15:
+    realday="Monday"
+elif day==16:
+    realday="Tuesday"
+print("Press 0 to send a mail to all the HODs and schedule a meeting on the day and time specified\n"      
+"\n1-Safety training department Mr Rajesh jain on "+realday+" at 3.30pm to 4.30pm\n")
+day=day+1
+if day==7 or day==14:
+    day=day+1
+if day==1:
+    realday="Monday"              
+elif day==2:
+    realday="Tuesday"
+elif day==3:
+    realday="Wednesday"
+elif day==4:
+    realday="Thursday"
+elif day==5:
+    realday="Friday"
+elif day==6:
+    realday="Saturday"
+elif day==8:
+    realday="Monday"
+elif day==9:
+    realday="Tuesday"
+elif day==10:
+    realday="Wednesday"
+elif day==11:
+    realday="Thursday"
+elif day==12:
+    realday="Friday"
+elif day==13:
+    realday="Saturday"
+elif day==15:
+    realday="Monday"
+elif day==16:
+    realday="Tuesday"
+print("2-CSR introduction department Mr Harish dari on "+realday+" at 12pm to 1pm\n"
+"3-Visit to secuirity and administration department Mr Rahul Raju on "+realday+" at 2pm to 3.30pm\n"
+"4-Visit to Viscose department Mr Ashwani Shukla on "+realday+" at 3.30pm to 5pm\n")
+day=day+1
+if day==7 or day==14:
+    day=day+1
+if day==1:
+    realday="Monday"              
+elif day==2:
+    realday="Tuesday"
+elif day==3:
+    realday="Wednesday"
+elif day==4:
+    realday="Thursday"
+elif day==5:
+    realday="Friday"
+elif day==6:
+    realday="Saturday"
+elif day==8:
+    realday="Monday"
+elif day==9:
+    realday="Tuesday"
+elif day==10:
+    realday="Wednesday"
+elif day==11:
+    realday="Thursday"
+elif day==12:
+    realday="Friday"
+elif day==13:
+    realday="Saturday"
+elif day==15:
+    realday="Monday"
+elif day==16:
+    realday="Tuesday"
+print("5-Visit to IT department Mr Hanuman Dubey on "+realday+" at 9am to 10am\n"
+"6-Visit to Legal department Mr Lucky Popli on "+realday+" at 10am to 11am\n"
+"7-Visit to Supply chain Management/Stores Mr Aman Prasad on "+realday+" at 11am to 12pm\n"
+"8-Visit to Purchase Department Mr Sachin Kanstiya on "+realday+" at 12pm to 1pm\n"
+"9-Visit to GST/Insurance department Mr Amber Bohra on "+realday+" at 2pm to 3.30pm\n"
+"10-Visit to Accounts department Mr Govind Kokra on "+realday+" at 3.30pm to 5pm\n")
+day=day+1
+if day==7 or day==14:
+    day=day+1
+if day==1:
+    realday="Monday"              
+elif day==2:
+    realday="Tuesday"
+elif day==3:
+    realday="Wednesday"
+elif day==4:
+    realday="Thursday"
+elif day==5:
+    realday="Friday"
+elif day==6:
+    realday="Saturday"
+elif day==8:
+    realday="Monday"
+elif day==9:
+    realday="Tuesday"
+elif day==10:
+    realday="Wednesday"
+elif day==11:
+    realday="Thursday"
+elif day==12:
+    realday="Friday"
+elif day==13:
+    realday="Saturday"
+elif day==15:
+    realday="Monday"
+elif day==16:
+    realday="Tuesday"
+print("11-Visit to Instrumentation department Mr Surendra Gautam on "+realday+" at 9am to 10am\n"
+"12-Visit to Laboratory-Introduction and Critical Equipments Mr Dharmesh Patel on "+realday+" at 10am to 12pm\n"
+"13-Visit to Electrical department Mr Shivratan Sethia on "+realday+" at 12am to 1pm\n"
+"14-Visit to Technical Cell Mr Manish Patel on "+realday+" at 2pm to 3.30pm\n"
+"15-Visit to Spinning-Introduction and Plant visit Mr S.K Sharma on "+realday+" at 3.30pm to 5pm\n")
+day=day+1
+if day==7 or day==14:
+    day=day+1
+if day==1:
+    realday="Monday"              
+elif day==2:
+    realday="Tuesday"
+elif day==3:
+    realday="Wednesday"
+elif day==4:
+    realday="Thursday"
+elif day==5:
+    realday="Friday"
+elif day==6:
+    realday="Saturday"
+elif day==8:
+    realday="Monday"
+elif day==9:
+    realday="Tuesday"
+elif day==10:
+    realday="Wednesday"
+elif day==11:
+    realday="Thursday"
+elif day==12:
+    realday="Friday"
+elif day==13:
+    realday="Saturday"
+elif day==15:
+    realday="Monday"
+elif day==16:
+    realday="Tuesday"
+print("16-Visit to IWM/CIVIL Mr Anand Singh on "+realday+" at 9am to 10am\n"
+"17-Visit to Workshop/NDT & Drawing Office Mr Hemraj Rawal on "+realday+" at 10am to 12pm\n"
+"18-Visit to Stores Department Mr Aman Prasad on "+realday+" at 12pm to 1pm\n"
+"19-Visit to CS2 / Acid  - Introduction & Plant Visit Mr Priyanshu Sinha on "+realday+" at 2pm to 3.30pm\n"
+"20-Visit to WTP / ETP Plant, Introduction & Plant Visit Mr Jitesh Soni on "+realday+" at 3.30pm to 5pm\n")
+day=day+1
+if day==7 or day==14:
+    day=day+1
+if day==1:
+    realday="Monday"              
+elif day==2:
+    realday="Tuesday"
+elif day==3:
+    realday="Wednesday"
+elif day==4:
+    realday="Thursday"
+elif day==5:
+    realday="Friday"
+elif day==6:
+    realday="Saturday"
+elif day==8:
+    realday="Monday"
+elif day==9:
+    realday="Tuesday"
+elif day==10:
+    realday="Wednesday"
+elif day==11:
+    realday="Thursday"
+elif day==12:
+    realday="Friday"
+elif day==13:
+    realday="Saturday"
+elif day==15:
+    realday="Monday"
+elif day==16:
+    realday="Tuesday"
+print("21-Visit to Aditya Birla Public School (ABPS) Ms Peu Chakroborty on "+realday+" at 9am to 10.30am\n"
+"22-Visit to Jankalyan Hospital Dr.Meghana Bhalerao on "+realday+" at 10.30am to 12pm\n"
+"23-Visit to TRADC Mr Mr.Abir Chakraborthy on "+realday+" at 12pm to 1pm\n"
+"24-Visit to Energy Centre Department Mr.Sanjay Tiwari on "+realday+" at 2pm to 3.30pm\n"
+"25-Visit to Recovery Department - Introduction & Plant Visit Mr.Tushar Wankhede on "+realday+" at 3.30pm to 5pm\n")
+day=day+1
+if day==7 or day==14:
+    day=day+1
+if day==1:
+    realday="Monday"              
+elif day==2:
+    realday="Tuesday"
+elif day==3:
+    realday="Wednesday"
+elif day==4:
+    realday="Thursday"
+elif day==5:
+    realday="Friday"
+elif day==6:
+    realday="Saturday"
+elif day==8:
+    realday="Monday"
+elif day==9:
+    realday="Tuesday"
+elif day==10:
+    realday="Wednesday"
+elif day==11:
+    realday="Thursday"
+elif day==12:
+    realday="Friday"
+elif day==13:
+    realday="Saturday"
+elif day==15:
+    realday="Monday"
+elif day==16:
+    realday="Tuesday"
+print("26-Visit to ABCFRC Mr.Saurabh Singh on "+realday+" at 9am to 10am\n"
+"27-Visit to HRM Mr.V.K.Sharma on "+realday+" at 10am to 11.30am\n"
+"28-Visit to F&C Mr.V.K.Sharma on "+realday+" at 11.30am to 1pm\n"
+"29-Visit to Energy Centre Dr.Surya kumar Singh on "+realday+" at 2pm to 3.30pm\n"
+"30-Visit to Technical department Mr. R. Ramkumar on "+realday+" at 3.30pm to 5pm\n")
+print("Press 0 to send a mail to all the HODs and schedule a meeting on the day and time specified above\n")      
+msg = MIMEText(u'<span><a href="mailto:nanuyadav911@gmail.com?subject=Re:Induction of the new joinee&body=This is to inform you that the meeting can be conducted on the day and time assigned.">Approve</a></span><span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><a href="mailto:nanuyadav911@gmail.com?subject=Re:Induction of the new joinee&body=Please reschedule the meeting on ">Reschedule</a><p>&nbsp;</p>','html')
+msg2 = msg.as_string()
+
+def mail(id,days,time,msg2):
+    import smtplib
+    
+    server=smtplib.SMTP('smtp.gmail.com',587)
+    server.starttls()
+    server.login('nanuyadav911@gmail.com','Anurag@123')
+    server.sendmail('nanuyadav911@gmail.com',id,msg2+'Dear all,<br><br>This is with reference to the induction of '+detail+'<br><br>Meeting with the candidate is scheduled on '+days+time+'<br><br>Request you to extend your full support to make this program successful.<br><br>Kind Regards.')
+
+choice=int(input("Enter your choice\n"))
+if choice is 0:
+    mail('f20160790@goa.bits-pilani.ac.in',realday,'at 3.30pm to 4.30pm',msg2)
+    print("\n_____Thank you_____\n Meeting with Mr Rajesh jain is scheduled on "+realday+" at 3.30pm to 4.30pm\n")
+    day=day+1
+    if day==7 or day==14:
+        day=day+1
+    if day==1:
+        realday="Monday"              
+    elif day==2:
+        realday="Tuesday"
+    elif day==3:
+        realday="Wednesday"
+    elif day==4:
+        realday="Thursday"
+    elif day==5:
+        realday="Friday"
+    elif day==6:
+        realday="Saturday"
+    elif day==8:
+        realday="Monday"
+    elif day==9:
+        realday="Tuesday"
+    elif day==10:
+        realday="Wednesday"
+    elif day==11:
+        realday="Thursday"
+    elif day==12:
+        realday="Friday"
+    elif day==13:
+        realday="Saturday"
+    elif day==15:
+        realday="Monday"
+    elif day==16:
+        realday="Tuesday"    
+    mail('f20160790@goa.bits-pilani.ac.in',realday,'at 12pm to 1pm',msg2)
+    print("\n_____Thank you_____\n Meeting with Mr Harish Dari is scheduled on "+realday+" at 12am to 1pm\n")
+    mail('f20160790@goa.bits-pilani.ac.in',realday,'at 2pm to 3.30pm',msg2)
+    print("\n_____Thank you_____\n Meeting with Mr Rahul Raju  is scheduled on "+realday+" at 2pm to 3.30pm\n")
+    mail('f20160790@goa.bits-pilani.ac.in',realday,'at 3.30pm to 5pm',msg2)
+    print("\n_____Thank you_____\n Meeting with Mr Ashwani Shukla is scheduled on "+realday+" at 3.30pm to 5pm\n")
+    day=day+1
+    if day==7 or day==14:
+        day=day+1
+    if day==1:
+        realday="Monday"              
+    elif day==2:
+        realday="Tuesday"
+    elif day==3:
+        realday="Wednesday"
+    elif day==4:
+        realday="Thursday"
+    elif day==5:
+        realday="Friday"
+    elif day==6:
+        realday="Saturday"
+    elif day==8:
+        realday="Monday"
+    elif day==9:
+        realday="Tuesday"
+    elif day==10:
+        realday="Wednesday"
+    elif day==11:
+        realday="Thursday"
+    elif day==12:
+        realday="Friday"
+    elif day==13:
+        realday="Saturday"
+    elif day==15:
+        realday="Monday"
+    elif day==16:
+        realday="Tuesday"    
+    mail('f20160790@goa.bits-pilani.ac.in',realday,'9am to 10am',msg2)
+    print("\n_____Thank you_____\n Meeting with Mr Hanuman Dubey is scheduled on "+realday+" at 9am to 10am\n")
+    mail('f20160790@goa.bits-pilani.ac.in',realday,'10am to 11am',msg2)
+    print("\n_____Thank you_____\n Meeting with Mr Lucky Popli is scheduled on "+realday+" at 10am to 11am\n")
+    mail('f20160790@goa.bits-pilani.ac.in',realday,'11am to 12am',msg2)
+    print("\n_____Thank you_____\n Meeting with Mr Aman Prasad is scheduled on "+realday+" at 11am to 12am\n")
+    mail('f20160790@goa.bits-pilani.ac.in',realday,'12pm to 1pm',msg2)
+    print("\n_____Thank you_____\n Meeting with Mr Sachin Kanstiya is scheduled on "+realday+" at 12pm to 1pm\n")
+    mail('f20160790@goa.bits-pilani.ac.in',realday,'2pm to 3pm',msg2)
+    print("\n_____Thank you_____\n Meeting with Mr Amber Bohra is scheduled on "+realday+" at 2pm to 3pm\n")
+    mail('f20160790@goa.bits-pilani.ac.in',realday,'3pm to 5pm',msg2)
+    print("\n_____Thank you_____\n Meeting with Mr Govind Kakra is scheduled on "+realday+" at 3pm to 5pm\n")
+    day=day+1
+    if day==7 or day==14:
+        day=day+1
+    if day==1:
+        realday="Monday"              
+    elif day==2:
+        realday="Tuesday"
+    elif day==3:
+        realday="Wednesday"
+    elif day==4:
+        realday="Thursday"
+    elif day==5:
+        realday="Friday"
+    elif day==6:
+        realday="Saturday"
+    elif day==8:
+        realday="Monday"
+    elif day==9:
+        realday="Tuesday"
+    elif day==10:
+        realday="Wednesday"
+    elif day==11:
+        realday="Thursday"
+    elif day==12:
+        realday="Friday"
+    elif day==13:
+        realday="Saturday"
+    elif day==15:
+        realday="Monday"
+    elif day==16:
+        realday="Tuesday"    
+    mail('f20160790@goa.bits-pilani.ac.in',realday,'9am to 10am',msg2)
+    print("\n_____Thank you_____\n Meeting with Mr Surendra Gautam is scheduled on "+realday+" at 9am to 10am\n")
+    mail('f20160790@goa.bits-pilani.ac.in',realday,'10am to 12am',msg2)
+    print("\n_____Thank you_____\n Meeting with Mr Dharmesh Patel is scheduled on "+realday+" at 10am to 12am\n")
+    mail('f20160790@goa.bits-pilani.ac.in',realday,'12 am to 1pm',msg2)
+    print("\n_____Thank you_____\n Meeting with Mr Shivratan Sethia is scheduled on "+realday+" at 12 am to 1pm\n")
+    mail('f20160790@goa.bits-pilani.ac.in',realday,'2pm to 3.30pm',msg2)
+    print("\n_____Thank you_____\n Meeting with Mr Manish Patel is scheduled on "+realday+" at 2pm to 3.30pm\n")
+    mail('f20160790@goa.bits-pilani.ac.in',realday,'3pm to 5pm',msg2)
+    print("\n_____Thank you_____\n Meeting with Mr S.K Sharma is scheduled on "+realday+" at 3pm to 5pm\n")
+    day=day+1
+    if day==7 or day==14:
+        day=day+1
+    if day==1:
+        realday="Monday"              
+    elif day==2:
+        realday="Tuesday"
+    elif day==3:
+        realday="Wednesday"
+    elif day==4:
+        realday="Thursday"
+    elif day==5:
+        realday="Friday"
+    elif day==6:
+        realday="Saturday"
+    elif day==8:
+        realday="Monday"
+    elif day==9:
+        realday="Tuesday"
+    elif day==10:
+        realday="Wednesday"
+    elif day==11:
+        realday="Thursday"
+    elif day==12:
+        realday="Friday"
+    elif day==13:
+        realday="Saturday"
+    elif day==15:
+        realday="Monday"
+    elif day==16:
+        realday="Tuesday"
+    mail('f20160790@goa.bits-pilani.ac.in',realday,'9am to 10am',msg2)
+    print("\n_____Thank you_____\n Meeting with Mr Anand Singh is scheduled on "+realday+" at 9am to 10am\n")
+    mail('f20160790@goa.bits-pilani.ac.in',realday,'10am to 12pm',msg2)
+    print("\n_____Thank you_____\n Meeting with Mr Hemraj Raval is scheduled on "+realday+" at 10am to 12pm\n")
+    mail('f20160790@goa.bits-pilani.ac.in',realday,'12pm to 1pm',msg2)
+    print("\n_____Thank you_____\n Meeting with Mr Aman Prasad is scheduled on "+realday+" at 12pm to 1pm\n")
+    mail('f20160790@goa.bits-pilani.ac.in',realday,'2pm to 3.30pm',msg2)
+    print("\n_____Thank you_____\n Meeting with Mr Priyamshu Sinha is scheduled on "+realday+" at 2pm to 3.30pm\n")
+    mail('f20160790@goa.bits-pilani.ac.in',realday,'3.30pm to 5pm',msg2)
+    print("\n_____Thank you_____\n Meeting with Mr Jitesh Soni is scheduled on "+realday+" at 3.30pm to 5pm\n")
+    day=day+1
+    if day==7 or day==14:
+        day=day+1
+    if day==1:
+        realday="Monday"              
+    elif day==2:
+        realday="Tuesday"
+    elif day==3:
+        realday="Wednesday"
+    elif day==4:
+        realday="Thursday"
+    elif day==5:
+        realday="Friday"
+    elif day==6:
+        realday="Saturday"
+    elif day==8:
+        realday="Monday"
+    elif day==9:
+        realday="Tuesday"
+    elif day==10:
+        realday="Wednesday"
+    elif day==11:
+        realday="Thursday"
+    elif day==12:
+        realday="Friday"
+    elif day==13:
+        realday="Saturday"
+    elif day==15:
+        realday="Monday"
+    elif day==16:
+        realday="Tuesday"    
+    mail('f20160790@goa.bits-pilani.ac.in',realday,'9am to 10.30am',msg2)
+    print("\n_____Thank you_____\n Meeting with Mr Peu Chakraborty is scheduled on "+realday+" at 9am to 10.30am\n")
+    mail('f20160790@goa.bits-pilani.ac.in',realday,'10.30am to 12pm',msg2)
+    print("\n_____Thank you_____\n Meeting with Mr Meghana Bhalerao is scheduled on "+realday+" at 10.30am to 12pm\n")
+    mail('f20160790@goa.bits-pilani.ac.in',realday,'12pm to 1pm',msg2)
+    print("\n_____Thank you_____\n Meeting with Mr Abir Chakraborthy is scheduled on "+realday+" at 12pm to 1pm\n")
+    mail('f20160790@goa.bits-pilani.ac.in',realday,'2pm to 3.30pm',msg2)
+    print("\n_____Thank you_____\n Meeting with Mr Sanjay Tiwari is scheduled on "+realday+" at 2pm to 3.30pm\n")
+    mail('f20160790@goa.bits-pilani.ac.in',realday,'3.30pm to 5pm',msg2)
+    print("\n_____Thank you_____\n Meeting with Mr Tushar Wankhede is scheduled on "+realday+" at 3.30pm to 5pm\n")
+    day=day+1
+    if day==7 or day==14:
+        day=day+1
+    if day==1:
+        realday="Monday"              
+    elif day==2:
+        realday="Tuesday"
+    elif day==3:
+        realday="Wednesday"
+    elif day==4:
+        realday="Thursday"
+    elif day==5:
+        realday="Friday"
+    elif day==6:
+        realday="Saturday"
+    elif day==8:
+        realday="Monday"
+    elif day==9:
+        realday="Tuesday"
+    elif day==10:
+        realday="Wednesday"
+    elif day==11:
+        realday="Thursday"
+    elif day==12:
+        realday="Friday"
+    elif day==13:
+        realday="Saturday"
+    elif day==15:
+        realday="Monday"
+    elif day==16:
+        realday="Tuesday"
+    mail('f20160790@goa.bits-pilani.ac.in',realday,'9am to 10am',msg2)
+    print("\n_____Thank you_____\n Meeting with Mr Saurabh Singh is scheduled on "+realday+" at 9am to 10am\n")
+    mail('f20160790@goa.bits-pilani.ac.in',realday,'10am to 11.30am',msg2)
+    print("\n_____Thank you_____\n Meeting with Mr V.K. Sharma is scheduled on "+realday+" at 10am to 11.30am\n")
+    mail('f20160790@goa.bits-pilani.ac.in',realday,'11.30am to 1pm',msg2)
+    print("\n_____Thank you_____\n Meeting with Mr Vinod Yadav is scheduled on "+realday+" at 11.30am to 1pm\n")
+    mail('f20160790@goa.bits-pilani.ac.in',realday,'2pm to 3.30pm',msg2)
+    print("\n_____Thank you_____\n Meeting with Mr Surya Kumar Singh is scheduled on "+realday+" at 2pm to 3.30pm\n")
+    mail('f20160790@goa.bits-pilani.ac.in',realday,'3.30pm to 5pm',msg2)
+    print("\n_____Thank you_____\n Meeting with Mr R.Rajkumar is scheduled on "+realday+" at 3.30pm to 5pm\n")
